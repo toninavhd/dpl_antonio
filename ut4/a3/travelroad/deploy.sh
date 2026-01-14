b92f46a38a32bc48333ed034c66b14d3965ede47
toninavhd <tillidie9999@gmail.com>
2026-01-14 16:42:35 +0000

    scripto

---
+++ ut4/a3/travelroad/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ssh arkania "
+  cd $(dirname $0)
+  git pull
+
+  source .venv/bin/activate
+  pip install -r requirements.txt
+
+  # python manage.py migrate
+  # python manage.py collectstatic --no-input
+
+  supervisorctl restart travelroad
+"
