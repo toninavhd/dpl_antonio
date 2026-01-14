39f94312d3e6a4b3d5e691891b006b5403eecad1
toninavhd <tillidie9999@gmail.com>
2026-01-14 16:29:15 +0000

    scripto

--- ut4/a3/travelroad/places/urls.py
+++ ut4/a3/travelroad/places/urls.py
@@ -1,4 +1,6 @@
+from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
 
 from . import views
 
@@ -6,4 +8,5 @@ app_name = 'places'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', include('places.urls', 'places')),
 ]
