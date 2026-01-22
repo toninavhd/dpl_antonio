1402b34ec0a35fa86860bec6746848e66d59e112
toninavhd <tillidie9999@gmail.com>
2026-01-22 16:08:56 +0000

    feat(nginx): Adding nginx configuration

--- ut4/a2/travelroad_django/deploy.sh
+++ ut4/a2/travelroad_django/deploy.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Directorio del proyecto en el servidor remoto
-REMOTE_DIR="/home/toni/Documentos/dpl_antonio/ut4/a2/travelroad_django"
+ssh dplprod_antonio@10.102.19.40 "
+  set -e
+  cd /home/dplprod_antonio/dpl_antonio/ut4/a2/travelroad_django
 
-ssh arkania "
-  cd $REMOTE_DIR
   git pull
 
   source .venv/bin/activate
   pip install -r requirements.txt
 
-  # python manage.py migrate
-  # python manage.py collectstatic --no-input
+  #python manage.py migrate
 
   supervisorctl restart travelroad
 "
-
